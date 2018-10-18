e9de1b60bf837957206a2694233d4b72a1bdd40a
Sekh Mahadi <msekh36@gmail.com>
2018-10-18 16:22:40 +0600

    TreeMap Collection Example Completed...

---
+++ JAVA_Collection_TreMap/src/TreeMap_Collection_JAVA/CreateTreeMapExample.java
@@ -0,0 +1,18 @@
+package TreeMap_Collection_JAVA;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class CreateTreeMapExample {
+public static void main(String args[]) {
+	SortedMap<String, String > fileExtensions=new TreeMap<>();
+	fileExtensions.put("python", ".py");
+    fileExtensions.put("c++", ".cpp");
+    fileExtensions.put("kotlin", ".kt");
+    fileExtensions.put("golang", ".go");
+    fileExtensions.put("java", ".java");
+
+    // Printing the TreeMap (Output will be sorted based on keys)
+    System.out.println(fileExtensions);
+}
+}
